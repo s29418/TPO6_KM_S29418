9e865f278b5ca13cd03cc418e38a2e23ce4af4ab
Mikolaj Kulas <misionn5@gmail.com>
2024-05-30 15:51:19 +0200

    Presented list in table

--- src/zad1/java/BookServlet.java
+++ src/zad1/java/BookServlet.java
@@ -13,7 +13,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class BookServlet extends HttpServlet {
     private DataSource dataSource;
@@ -42,23 +42,70 @@ public class BookServlet extends HttpServlet {
     private void serviceRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
-        out.println("<h2>Lista dostępnych książek</h2>");
+        String searchTitle = req.getParameter("title");
+        String searchAuthor = req.getParameter("author");
 
-        try (Connection con = dataSource.getConnection();
-             Statement stmt = con.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM pozycje")) {
+        out.println("<h2>Lista książek</h2>");
+        out.println("<form method='GET'>");
+        out.println("Tytuł: <input type='text' name='title' />");
+        out.println("Autor: <input type='text' name='author' />");
+        out.println("<input type='submit' value='Szukaj' />");
+        out.println("</form>");
 
-            out.println("<ol>");
-            while (rs.next()) {
-                String tytul = rs.getString("tytul");
-                float cena = rs.getFloat("cena");
-                out.println("<li>" + tytul + " - cena: " + cena + "</li>");
+        try (Connection con = dataSource.getConnection()) {
+            String query = getQuery(searchTitle, searchAuthor);
+
+            try (PreparedStatement pstmt = con.prepareStatement(query)) {
+                int paramIndex = 1;
+                if (searchTitle != null && !searchTitle.isEmpty()) {
+                    pstmt.setString(paramIndex++, "%" + searchTitle + "%");
+                }
+                if (searchAuthor != null && !searchAuthor.isEmpty()) {
+                    pstmt.setString(paramIndex, "%" + searchAuthor + "%");
+                }
+
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    out.println("<table border='1'>");
+                    out.println("<tr><th>ISBN</th><th>Autor</th><th>Tytuł</th><th>Rok</th><th>Wydawca</th><th>Cena</th></tr>");
+                    while (rs.next()) {
+                        String isbn = rs.getString("ISBN");
+                        String tytul = rs.getString("TYTUL");
+                        String autor = rs.getString("AUTOR_NAME");
+                        String wydawca = rs.getString("WYDAWCA_NAME");
+                        int rok = rs.getInt("ROK");
+                        float cena = rs.getFloat("CENA");
+                        out.println("<tr><td>" + isbn + "</td><td>" + autor + "</td><td>" + tytul + "</td><td>" + rok + "</td><td>" + wydawca + "</td><td>" + cena + "</td></tr>");
+                    }
+                    out.println("</table>");
+                }
             }
-            out.println("</ol>");
         } catch (SQLException e) {
             out.println("Błąd podczas uzyskiwania danych: " + e.getMessage());
         } finally {
             out.close();
         }
+    }
+
+    private static String getQuery(String searchTitle, String searchAuthor) {
+        String query = "SELECT POZYCJE.ISBN, POZYCJE.TYTUL, POZYCJE.CENA, POZYCJE.ROK, AUTOR.NAME as AUTOR_NAME, WYDAWCA.NAME as WYDAWCA_NAME " +
+                "FROM POZYCJE " +
+                "JOIN AUTOR ON POZYCJE.AUTID = AUTOR.AUTID " +
+                "JOIN WYDAWCA ON POZYCJE.WYDID = WYDAWCA.WYDID";
+
+        if (searchTitle != null && !searchTitle.isEmpty() || searchAuthor != null && !searchAuthor.isEmpty()) {
+            query += " WHERE";
+            boolean addAnd = false;
+            if (searchTitle != null && !searchTitle.isEmpty()) {
+                query += " POZYCJE.TYTUL LIKE ?";
+                addAnd = true;
+            }
+            if (searchAuthor != null && !searchAuthor.isEmpty()) {
+                if (addAnd) {
+                    query += " AND";
+                }
+                query += " AUTOR.NAME LIKE ?";
+            }
+        }
+        return query;
     }
 }
